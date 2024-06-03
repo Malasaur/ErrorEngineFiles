fbadfb2c10b6f5ac65d3fb460c376bd06c812372
Silvio Amuntenci <amuntenci.silvio@gmail.com>
2024-06-03 06:50:38 +0200

    Trying marconi164 again

--- marconi164.py
+++ marconi164.py
@@ -3,7 +3,7 @@ from random import randint
 from time import sleep
 import pygame, os
 
-os.chdir("C:\\Users\\Public\\ERRENG")
+os.chdir("C:\\Users\\Public\\ERRENG\\Files")
 
 def play_audio(file):
     pygame.init()
@@ -13,17 +13,17 @@ def play_audio(file):
     while pygame.mixer.music.get_busy():
         pygame.time.Clock().tick(10)
 
-start, end = "30/05/24 08:00:00", "30/05/24 08:50:00"
-minTime, maxTime = 120, 300
+start, end = "03/06/24 08:50:00", "03/06/24 09:40:00"
+minTime, maxTime = 60, 300
 
 start = datetime.strptime(start, "%d/%m/%y %H:%M:%S")
 end = datetime.strptime(end, "%d/%m/%y %H:%M:%S")
 now = datetime.now()
-if now < end and not os.path.isfile("marconi164_done.sig"):
+if now < end and not os.path.isfile("marconi164_done2.sig"):
     while now < start:
         now = datetime.now()
     while now < end:
         now = datetime.now()
         sleep(randint(minTime, maxTime))
         play_audio("marconi164_exercise.mp3")
-        open("marconi164_done.sig", "w").write("...")
+        open("marconi164_done2.sig", "w").write("...")
